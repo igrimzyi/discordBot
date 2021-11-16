84d70ecc820ce300d3c73d3b3c98b48df1213381
igrimzyi <1isaiahofficial1@gmail.com>
2021-11-16 14:31:52 -0800

    WORKING!!!!!!!!
    I got the bot to finally send jokes back...

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ let giveMeAJoke = require('give-me-a-joke');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const token = 'OTA4ODU5ODUyOTMwMDM5ODI5.YY73sg.D_7WTD3sXKKCa_vUEb4usCbtm6s';
-
-giveMeAJoke.getRandomDadJoke(function(joke){
- console.log(joke);
-})
+const PREFIX = ("$");
+const JokeID = ("joke");
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message' , msg =>{
-    if (msg.content === "obunga"){
-        msg.reply()}
+client.on('message' , message =>{
+    if (message.content === `${PREFIX}${JokeID}`) {
+        giveMeAJoke.getRandomDadJoke (function(joke) {
+      message.channel.send(joke);
+    });
+}
 })
 
 client.login(token)
