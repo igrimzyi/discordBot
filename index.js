6d35126e6dfa4cc24d3529b695b4e5f24f50fe7f
igrimzyi <1isaiahofficial1@gmail.com>
2021-11-16 13:51:56 -0800

    added a new node Module
    Still figuring out how to respond with actual joke

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ let giveMeAJoke = require('give-me-a-joke');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const token = 'OTA4ODU5ODUyOTMwMDM5ODI5.YY73sg.D_7WTD3sXKKCa_vUEb4usCbtm6s';
 
-let dadJokes = giveMeAJoke.getRandomDadJoke(function (joke){
- return joke; 
+giveMeAJoke.getRandomDadJoke(function(joke){
+ console.log(joke);
 })
+
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message' , msg =>{
-    if (msg.content === "$dadJoke"){
-        dadJokes().then(quote => msg.channel.send(quote))
-    }
+    if (msg.content === "obunga"){
+        msg.reply()}
 })
 
 client.login(token)
