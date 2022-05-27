fa974155a9750ab9f0fb331c38d86e16822e7f07
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-05-27 15:58:21 -0700

    update react message :white_check_mark:

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const token = process.env.TOKEN;
 
 
 
-levels.setURL("mongodb+srv://bot:puWZ87TdL5FtBhDJ@cluster0.pm1sa.mongodb.net/Cluster0?retryWrites=true&w=majority")
+// levels.setURL("mongodb+srv://bot:puWZ87TdL5FtBhDJ@cluster0.pm1sa.mongodb.net/Cluster0?retryWrites=true&w=majority")
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = ("$");
@@ -22,33 +22,33 @@ client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', async message =>{
-    if (message.content === `${PREFIX}${JokeID}`|| `${PREFIX}${rankID}`){
-        message.react('')
+client.on('message', async messageCreate =>{
+    if (messageCreate.content === `${PREFIX}${JokeID}`|| `${PREFIX}${rankID}`){
+        messageCreate.react("\u2764")
     }
 })
 
-client.on('message', async message =>{
-    if (!message.guild) return;
-    if (message.author.bot) return;
+// client.on('message', async message =>{
+//     if (!message.guild) return;
+//     if (message.author.bot) return;
 
-    const randomXp = Math.floor(Math.random()* 9)+ 1 ;
+//     const randomXp = Math.floor(Math.random()* 9)+ 1 ;
 
-    const hasLeveledUp = await levels.appendXp(message.author.id, message.guild.id, randomXp);
-    if (hasLeveledUp){
-        const user = await levels.fetch(message.author.id, message.guild.id);
-        message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
-    }
-    if (message.content === `${PREFIX}${rankID}`){
-        const user = await levels.fetch(message.author.id, message.guild.id);
-        message.channel.send(`You are currently level **${user.level}**!`)
-    }
-})
+//     const hasLeveledUp = await levels.appendXp(message.author.id, message.guild.id, randomXp);
+//     if (hasLeveledUp){
+//         const user = await levels.fetch(message.author.id, message.guild.id);
+//         message.channel.send(`You leveled up to ${user.level}! Keep it going!`);
+//     }
+//     if (message.content === `${PREFIX}${rankID}`){
+//         const user = await levels.fetch(message.author.id, message.guild.id);
+//         message.channel.send(`You are currently level **${user.level}**!`)
+//     }
+// })
 
-client.on('message' , message =>{
-    if (message.content === `${PREFIX}${JokeID}`) {
+client.on('message' , async messageCreate =>{
+    if (messageCreate.content === `${PREFIX}${JokeID}`) {
         giveMeAJoke.getRandomDadJoke (function(joke) {
-      message.channel.send(joke);
+      messageCreate.channel.send(joke);
     });
 }
 })
