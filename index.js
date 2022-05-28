e79d5d5657f09420e93311bf30df40b49300afa4
Isaiah Tamayo <1isaiahofficial1@gmail.com>
2022-05-28 12:52:16 -0700

    Bug fix :white_check_mark:

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
+// Setting env variables
+const express = require('express')
+const app = express()
 const { Client, Intents } = require('discord.js');
 let giveMeAJoke = require('give-me-a-joke');
 const levels = require('discord-xp');
 require('dotenv').config();
-
 const token = process.env.TOKEN;
-
-
-
 levels.setURL(process.env.MONGOURI)
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const PREFIX = ("$");
 const JokeID = ("joke");
 const rankID = ("rank");
+const port = 3000;
 
 
+// functions
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('message', async messageCreate =>{
-    if (messageCreate.content === `${PREFIX}${JokeID}`|| `${PREFIX}${rankID}`){
+// ranks
+client.on('messageCreate', async messageCreate =>{
+    if (messageCreate.content.substring(0,1)=== '$' ){
         messageCreate.react("\u2764")
     }
 })
 
-client.on('message', async messageCreate =>{
+// levels 
+client.on('messageCreate', async messageCreate =>{
     if (!messageCreate.guild) return;
     if (messageCreate.author.bot) return;
 
@@ -34,20 +36,37 @@ client.on('message', async messageCreate =>{
     const hasLeveledUp = await levels.appendXp(messageCreate.author.id, messageCreate.guild.id, randomXp);
     if (hasLeveledUp){
         const user = await levels.fetch(messageCreate.author.id, messageCreate.guild.id);
-        messageCreate.channel.send(`You leveled up to ${user.level}! Keep it going!`);
+        messageCreate.channel.send(`You leveled up to level ${user.level}! Keep it going!`);
     }
     if (messageCreate.content === `${PREFIX}${rankID}`){
         const user = await levels.fetch(messageCreate.author.id, messageCreate.guild.id);
         messageCreate.channel.send(`You are currently level **${user.level}**!`)
     }
 })
-
-client.on('message' , async messageCreate =>{
+// random joke
+client.on('messageCreate' , async messageCreate =>{
     if (messageCreate.content === `${PREFIX}${JokeID}`) {
         giveMeAJoke.getRandomDadJoke (function(joke) {
       messageCreate.channel.send(joke);
     });
 }
 })
+client.on('messageCreate', async messageCreate =>{  
+    
+    if(messageCreate.content === `${PREFIX}doge`){
+        
+        app.get('https://dog.ceo/api/breed/shiba/images/random', (req,res)=>{
+            console.log('hello')
+         })
+
+        // messageCreate.channel.send("Here is me!", {files: ["https://dog.ceo/api/breed/Shiba/images/random"] });
+    }
+
+})
+
+
 
 client.login(token)
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+  })
